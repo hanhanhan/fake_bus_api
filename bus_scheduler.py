72e018f4937cfac9bffeb33aa67b019dbbda393b
Hannah Lazarus <hanhanhan.lazarus@gmail.com>
2017-12-21 09:59:41 -0800

    sketch schedule for one bus route

--- bus_scheduler.py
+++ bus_scheduler.py
@@ -1,17 +1,18 @@
 from time import time
 from itertools import cycle
+import sched, time
 
 bus_stops = range(1, 11)
 # routes servicing bus_stops
 routes = [1, 2, 3]
 
 # Minutes interval between sending buses on  route
-same_route_start_interval = 15
-different_route_start_interval = 2
-drive_time_between_stops = 2
+same_route_start_interval = minutes_to_epoch(15)
+different_route_start_interval = minutes_to_epoch(2)
+drive_time_between_stops = minutes_to_epoch(2)
 
 # create a dictionary of buses, continually update for time
-
+scheduler = sched.scheduler()
 
 def minutes_to_epoch(minutes):
     return time.gmtime(minutes * 60)
@@ -23,26 +24,21 @@ class Bus:
         self.route = route
         self.time = start_time
         self.bus_stop_time = start_time
-        self.in_service = True
+        self.on_route = True
 
+    def move(self, cur_time):
 
-    def __iter__(self, time):
+        if self.bus_stop >= 10:
+            self.on_route = False
+            return
 
-        for bus_stop in cycle(bus_stops):
-            self.stop = bus_stop
+        if cur_time - self.bus_stop_time >= drive_time_between_stops:
+            self.stop += 1
             self.stop_time += drive_time_between_stops
-            yield (self.stop, self.bus_stop_time)
 
 
-class Stop:
 
-    def __init__(self, stop_id, buses):
-        self.stop_id = stop_id
-        self.buses = buses
 
-    def get_next_buses(self, time):
-
-        return bus
 
 """
 pass in stop id, time
