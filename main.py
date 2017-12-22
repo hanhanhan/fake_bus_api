7abe928ec5f3f0e896e04f1a1502f449902114ed
Hannah Lazarus <hanhanhan.lazarus@gmail.com>
2017-12-22 02:23:47 -0800

    add 404 header response

--- main.py
+++ main.py
@@ -2,10 +2,9 @@
 import json
 import time
 # Third Party Libraries
-from flask import Flask
+from flask import Flask, make_response
 from flask_socketio import SocketIO
 # Project Functions
-from bus_schedule_builder import make_schedule
 from helper_functions import make_stop_filtered_schedule
 
 
@@ -13,20 +12,26 @@ from helper_functions import make_stop_filtered_schedule
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-# Route
+# View + Path
 @app.route('/<stop_id>')
-def hello(stop_id):
+def bus_stop_schedule(stop_id):
+
     try:
         stop_id = int(stop_id)
     except ValueError:
-             return '404 Not Found - Use Bus Stop ID from 1 to 10'
+        return error_response()
 
     if stop_id not in range(1, 11):
-        return '404 Not Found - Use Bus Stop ID from 1 to 10'
+        return error_response()
 
     minutes_timestamp = time.gmtime(time.time()).tm_min
     schedule = make_stop_filtered_schedule(minutes_timestamp, stop_id)
     return json.dumps(schedule)
+
+
+def error_response():
+    return make_response('404 Not Found - Use Bus Stop ID from 1 to 10', 404)
+
 
 if __name__ == '__main__':
     socketio.run(app)
