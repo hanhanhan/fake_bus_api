de900e62a0430d17868b4fed84900229bd6f5e4c
Hannah Lazarus <hanhanhan.lazarus@gmail.com>
2017-12-23 11:45:02 -0800

    remove socketio

--- main.py
+++ main.py
@@ -3,15 +3,12 @@ import json
 import time
 # Third Party Libraries
 from flask import Flask, make_response
-from flask_socketio import SocketIO
 # Project Functions
 from helper_functions import make_stop_filtered_schedule
 
 
 # Initialization
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
 
 # View + Path
 @app.route('/<stop_id>')
@@ -34,14 +31,6 @@ def error_response():
     return make_response('404 Not Found - Use Bus Stop ID from 1 to 10', 404)
 
 
-@socketio.on('json', namespace='/<int>')
-def text(message):
-    """Sent by a client when the user entered a new message.
-    The message is sent to all people in the room."""
-    room = session.get('room')
-    emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
-
-
 
 if __name__ == '__main__':
-    socketio.run(app)
+    app.run()
