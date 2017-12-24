6c1fc42f4ca75761c6142b91ea78c0f0146c586c
Hannah Lazarus <hanhanhan.lazarus@gmail.com>
2017-12-24 09:12:04 -0500

    stripped down for troubleshooting

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-# Built In Libraries
-import json
-import time
-# Third Party Libraries
+
 from flask import Flask, make_response
 from flask_socketio import SocketIO, emit, send
-# Project Functions
-from helper_functions import make_stop_filtered_schedule
 
 
 # Initialization
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-# @socketio.on('json')
-# def handle_json(json):
-#     print('received json: ' + str(json))
 
 @socketio.on('connect')
 def test_connect():
@@ -26,32 +18,13 @@ def test_connect():
 def handle_message(message):
     print('received message: ' + message)
 
+
 @app.route('/')
 def index():
-    return 'hi im the index'
-
-# View + Path
-# @app.route('/<stop_id>')
-# def bus_stop_schedule(stop_id):
-
-#     try:
-#         stop_id = int(stop_id)
-#     except ValueError:
-#         return error_response()
-
-#     if stop_id not in range(1, 11):
-#         return error_response()
-
-#     minutes_timestamp = time.gmtime(time.time()).tm_min
-#     schedule = make_stop_filtered_schedule(minutes_timestamp, stop_id)
-#     return json.dumps(schedule)
+    return 'hi i\'m the index'
 
 
-def error_response():
-    return make_response('404 Not Found - Use Bus Stop ID from 1 to 10', 404)
-
-
-@socketio.on('json', namespace='/<int>')
+@socketio.on('json', namespace='/living_room')
 def text(message):
     """Sent by a client when the user entered a new message.
     The message is sent to all people in the room."""
