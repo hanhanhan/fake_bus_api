7ad77109acb6cecd4a0eec4c5f28893d548d65bc
Hannah Lazarus <hanhanhan.lazarus@gmail.com>
2017-12-27 23:31:12 -0500

    get rid of extra endpoints

--- main.py
+++ main.py
@@ -18,54 +18,13 @@ app.debug = True
 @app.route('/')
 def full_schedule():
     schedule = make_schedule()
-    return json.dumps(schedule)
+    timestamp = time.gmtime(time.time()).tm_min
+    api_data = {'schedule': schedule, 'timestamp': timestamp}
 
-
-@app.route('/api/<stop_id>')
-def bus_stop_schedule(stop_id):
-
-    try:
-        stop_id = int(stop_id)
-    except ValueError:
-        return error_response()
-
-    if stop_id not in range(1, 11):
-        return error_response()
-
-    minutes_timestamp = time.gmtime(time.time()).tm_min
-    schedule = make_stop_filtered_schedule(minutes_timestamp, stop_id)
-    
-    response = make_response(json.dumps(schedule))
+    response = make_response(json.dumps(api_data))
     response.headers['Access-Control-Allow-Origin'] = '*'
+
     return response
-
-
-def error_response():
-    return make_response('404 Not Found - Use Bus Stop ID from 1 to 10', 404)
-
-
-@app.route('/client')
-def pretend_client():
-    stop_ids = [1, 2]
-    schedule = {}
-    
-    for stop_id in stop_ids:
-        minutes_timestamp = time.gmtime(time.time()).tm_min
-        stop_schedule = make_stop_filtered_schedule(minutes_timestamp, stop_id)
-        schedule[stop_id] = format_(stop_schedule)
-
-    return render_template('client.html', schedule=schedule, timestamp=time.time())
-
-
-def format_(routes_schedule):
-    formatted = {}
-    
-    for route, minutes in routes_schedule.items():
-        route_match = re.search('\w(\d*)', route)
-        route_id = route_match.groups()[0]
-        formatted[route_id] = f'{minutes[0]} min, {minutes[1]} min'
-    
-    return formatted
 
 
 if __name__ == '__main__':
